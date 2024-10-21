62ed99ba96b7d56cb3c12e02f4eddd917ee74777
Yusufcatursaputro <161544056+Yusufcatursaputro@users.noreply.github.com>
2024-10-21 11:41:12 +0700

    "menambah ubah password"

--- resources/views/auth/reset.blade.php
+++ resources/views/auth/reset.blade.php
@@ -9,7 +9,6 @@
     @vite('resources/css/app.css')
 </head>
 
-
 <nav>
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -23,39 +22,37 @@
 <body>
     <section>
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
                 <img class="w-20 h-20 mr-2" src="src/Logo.png"
                     alt="logo">
             </a>
-            <div
-                class="w-full rounded-full shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            
+            <div class="w-full rounded-full shadow dark:border sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div class="p-6 rounded-2xl space-y-4 md:space-y-6 sm:p-8">
-                    <h1
-                        class="text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Masuk ke Web Pengelolaan Keuangan
-                    </h1>
-                    <form class="space-y-4 md:space-y-6" action="#">
+                <h1 class="text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Ubah Password Baru
+            </h1>
+                    <form class="space-y-4 md:space-y-6"  method="POST">
+                        @csrf
+                        <input type="hidden" name="token" >
                         <div>
                             <input type="email" name="email" id="email"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="email" required="">
+                                placeholder="Email" required>
                         </div>
                         <div>
-                            <input type="password" name="password" id="password" placeholder="••••••••"
+                            <input type="password" name="password" id="password"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                required="">
+                                placeholder="Password Baru" required>
                         </div>
-                        <div class="flex items-center justify-between">
-                            <div class="flex items-start">
-                                
-                            </div>
-                            <a href="#"
-                                class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Ubah
-                                password?</a>
+                        <div>
+                            <input type="password" name="password_confirmation" id="password_confirmation"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Konfirmasi Password Baru" required>
                         </div>
                         <button type="submit"
                             class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                            Login</button>
+                            Ubah Password</button>
                     </form>
                 </div>
             </div>
@@ -63,14 +60,11 @@
     </section>
 </body>
 
-
-
 <footer>
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <span class="block text-sm sm:text-center">© 2023 <a
                 href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
     </div>
 </footer>
-
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/forgot-password', function () {
+    return view('auth.reset');
+});
+
+// Route::post('/forgot-password', [PasswordResetController::class, 'sendResetLink'])->name('password.email');
