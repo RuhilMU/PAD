f68145c843355aa46cde52eed94ad107a3dcc710
Yusufcatursaputro <161544056+Yusufcatursaputro@users.noreply.github.com>
2024-11-03 14:00:29 +0700

    edit tabel barang dan pegawai

--- resources/views/barang/barang.blade.php
+++ resources/views/barang/barang.blade.php
@@ -32,7 +32,7 @@
     <section class="p-9">
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <thead class="text-xs text-white uppercase bg-[#324150] dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="px-6 py-3">
                             Tanggal
