43642771e4c9f39aad0f4c991941bb72c11b2f7c
Yusufcatursaputro <161544056+Yusufcatursaputro@users.noreply.github.com>
2024-11-08 13:44:39 +0700

    iya

--- resources/views/barang/barang.blade.php
+++ resources/views/barang/barang.blade.php
@@ -6,54 +6,53 @@
 </head>
 
 @section('content')
-
-<section class="px-60 py-20">
-    <!-- tabel barang -->
+<!-- tabel barang -->
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg mx-28 mt-10">
     <div class="flex items-center" style="background:#EEF0F4">
         <a href="{{ route('barang.create')}}" class="col p-3 items-center text-blue-600 hover:underline" style="font-weight:bold; font-size:13px">Tambah</a>
         <a href="/edit-barang" class="col p-3 items-center text-blue-600 hover:underline" style="font-weight:bold; font-size:13px">Edit</a>
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500">
-            <thead class="text-xs text-white uppercase bg-[#324150]">
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                        Tanggal
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Total Harga
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Keterangan
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Action
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($data_keluar as $expense)
-                <tr class="bg-white border-b">
-
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                        {{ \Carbon\Carbon::parse($expense->date)->format('d-m-Y') }}
-                    </th>
-                    <td class="px-6 py-4">
-                        {{ 'Rp ' . number_format($expense->amount, 2, ',', '.') }}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{ $expense->description }}
-                    </td>
-                    <td class="flex items-center px-6 py-4">
-                        <a href="{{ route('barang.edit', $expense->expense_id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                        <form action="{{ route('barang.destroy', $expense->expense_id) }}" method="POST" style="display:inline;">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3" style="border: none; background: none; cursor: pointer;">Remove</button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
     </div>
-</section>
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+        <thead class="text-xs text-white uppercase bg-[#324150]">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Tanggal
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Total Harga
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Keterangan
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($data_keluar as $expense)
+            <tr class="bg-white border-b">
+
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                    {{ \Carbon\Carbon::parse($expense->date)->format('d-m-Y') }}
+                </th>
+                <td class="px-6 py-4">
+                    {{ 'Rp ' . number_format($expense->amount, 2, ',', '.') }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $expense->description }}
+                </td>
+                <td class="flex items-center px-6 py-4">
+                    <a href="{{ route('barang.edit', $expense->expense_id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                    <form action="{{ route('barang.destroy', $expense->expense_id) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3" style="border: none; background: none; cursor: pointer;">Remove</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
--- resources/views/manajemen/pegawai.blade.php
+++ resources/views/manajemen/pegawai.blade.php
@@ -8,18 +8,22 @@
 @section('content')
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg mx-28 mt-10">
     <div class="flex items-center justify-between" style="background:#EEF0F4">
-        <div class="col p-3">
-            <a href="{{ route('pegawai.create')}}" class="col items-center text-blue-600 hover:underline" style="font-weight:bold; font-size:13px">Tambah</a>
-            <a href="/edit-barang" class="col p-3 items-center text-blue-600 hover:underline" style="font-weight:bold; font-size:13px">Edit</a>
+        <div class="col">
+            <a href="{{ route('pegawai.create')}}" class="col pl-3 items-center text-blue-600 hover:underline" style="font-weight:bold; font-size:13px">Tambah</a>
+            <a href="/edit-barang" class="col pl-3 items-center text-blue-600 hover:underline" style="font-weight:bold; font-size:13px">Edit</a>
         </div>
-        <div class="relative col p-3">
-            <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                </svg>
-            </div>
+        <div class="relative">
             <form action="{{ route('pegawai.search') }}" method="GET">
-                <input type="text" id="table-search" class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-72 bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search">
+                <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+                <div class="relative py-3">
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
+                    </div>
+                    <input type="search" id="search" class="mr-3 block w-72 h-5 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search" required />
+                    
+                </div>
             </form>
         </div>
     </div>
@@ -45,11 +49,11 @@
         </thead>
         <tbody>
             @foreach($data_user as $index => $user)
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <tr class="bg-white border-b hover:bg-gray-50">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowra">
                     {{ $index+1 }}
                 </th>
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                     {{ $user->name }}
                 </th>
                 <td class="px-6 py-4">
@@ -60,11 +64,11 @@
                 </td>
 
                 <td class="px-6 py-4">
-                    <a href="{{ route('pegawai.edit', $user->user_id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                    <a href="{{ route('pegawai.edit', $user->user_id) }}" class="font-medium text-blue-600 hover:underline">Edit</a>
                     <form action="{{ route('pegawai.destroy', $user->user_id) }}" method="POST" onsubmit="return confirmDelete();">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</button>
+                        <button type="submit" class="font-medium text-blue-600 hover:underline">Delete</button>
                     </form>
                 </td>
             </tr>
