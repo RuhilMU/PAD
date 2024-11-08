ec51dd5c5dd466b94531f80f4b4616e65410abfd
Yusufcatursaputro <161544056+Yusufcatursaputro@users.noreply.github.com>
2024-11-08 15:43:22 +0700

    tambahan input

--- resources/views/transaksi/tambah.blade.php
+++ resources/views/transaksi/tambah.blade.php
@@ -17,6 +17,9 @@
                 @csrf
                 <input type="text" name="nama_barang" id="nama_barang"
                     class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                    placeholder="Nama Toko" required="">
+                <input type="text" name="nama_barang" id="nama_barang"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                     placeholder="Nama Barang" required="">
                 <input type="text" name="tanggal_masuk" id="tanggal_masuk"
                     class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
@@ -31,7 +34,7 @@
                     class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                     placeholder="Stok" required="">
                 <button type="submit"
-                    class="bg-[#4C7DE7] w-full place-items-center text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                    class="bg-[#4C7DE7] ml-28 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5">
                     Simpan</button>
             </form>
         </div>
