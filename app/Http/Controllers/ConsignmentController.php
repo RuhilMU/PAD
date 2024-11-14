e6602ec12e9d8692f85272522229723b65336180
RuhilMU <ruhilmuhammadukasyah2005@mail.ugm.ac.id>
2024-11-14 21:15:55 +0700

    fix kongxi

--- app/Http/Controllers/ConsignmentController.php
+++ app/Http/Controllers/ConsignmentController.php
@@ -15,13 +15,13 @@ class ConsignmentController extends Controller
         $consignments = Consignment::with('product', 'store')
         ->get()
         ->map(function ($consignment) {
-            $status = $consignment->exit_date ? 'Close' : 'Open';
+            $status = $consignment->entry_date ? 'Close' : 'Open';
 
             $circulationDuration = $consignment->entry_date && $consignment->exit_date 
-                ? Carbon::parse($consignment->entry_date)->diffInDays(Carbon::parse($consignment->exit_date)) 
+                ? Carbon::parse($consignment->exit_date)->diffInDays(Carbon::parse($consignment->entry_date)) 
                 : null;
 
-            $totalPrice = $consignment->quantity * $consignment->product->price;
+            $totalPrice = $consignment->sold * $consignment->product->price;
 
             return [
                 'consignment_id' => $consignment->consignment_id,
@@ -34,6 +34,7 @@ class ConsignmentController extends Controller
                 'price' => $consignment->product->price,
                 'total_price' => $totalPrice,
                 'quantity' => $consignment->quantity,
+                'sold' => $consignment->sold,
             ];
         });
 
@@ -51,66 +52,62 @@ class ConsignmentController extends Controller
         return view('transaksi.tambah');
     }
 
-        public function laporanStore(Request $request)
-        {
-            $request->validate([
-                'product_name' => 'required|string',
-                'store_name' => 'required|string',
-                'price' => 'required|integer',
-                'quantity' => 'required|integer',
-                'entry_date' => 'required|date',
-                'exit_date' => 'nullable|date',
-            ]);
+    public function laporanStore(Request $request)
+    {
+        $request->validate([
+            'product_name' => 'required|string',
+            'store_name' => 'required|string',
+            'price' => 'required|integer',
+            'quantity' => 'required|integer',
+            'exit_date' => 'required|date',
+        ]);
 
-            $consignment = new Consignment();
+        $consignment = new Consignment();
             
-            $product = Product::create(['product_name'=>$request->product_name, 'price'=>$request->price]);
-            $store = Store::create(['store_name'=>$request->store_name]);
+        $product = Product::create(['product_name'=>$request->product_name, 'price'=>$request->price]);
+        $store = Store::create(['store_name'=>$request->store_name]);
 
-            $consignment->product_id = $product->product_id;
-            $consignment->store_id = $store->store_id;
-            $consignment->quantity = $request->quantity;
-            $consignment->entry_date = $request->entry_date;
+        $consignment->product_id = $product->product_id;
+        $consignment->store_id = $store->store_id;
+        $consignment->quantity = $request->quantity;
+        $consignment->exit_date = $request->exit_date;
 
-            if ($request->has('exit_date')) {
-                $consignment->exit_date = $request->exit_date;
-            }
-            $consignment->save();
-            return redirect('/transaksi');
+        $consignment->save();
+        return redirect('/transaksi');
         }
 
     
 
-    public function laporanUpdate(Request $request, $consignment_id)
-    {
-        $request->validate([
-            'store_name' => 'required|string',
-            'product_name' => 'required|string',
-            'quantity' => 'required|integer',
-            'price' => 'required|integer',
-            'entry_date' => 'required|date',
-            'exit_date' => 'nullable|date',
-        ]);
-
-        $consignment = Consignment::with(['store','product'])->find($consignment_id);
-
-        // $consignment->store->store_name = $request->store_name;
-        // $consignment->store->save();
-        $consignment->store->update(['store_name'=>$request->store_name]);
-
-        // $consignment->product->product_name = $request->product_name;
-        // $consignment->product->save();
-        $consignment->product->update(['product_name'=>$request->product_name]);
-        $consignment->product->update(['price'=>$request->price]);
-
-        $consignment->quantity = $request->quantity;
-        $consignment->entry_date = $request->entry_date;
-
-        if ($request->has('exit_date')) {
+        public function laporanUpdate(Request $request, $consignment_id)
+        {
+            $request->validate([
+                'store_name' => 'required|string',
+                'product_name' => 'required|string',
+                'entry_date' => 'required|date',
+                'exit_date' => 'required|date',
+                'price' => 'required|integer',
+                'sold' => 'required|integer',
+                'quantity' => 'required|integer',
+            ]);
+    
+            $consignment = Consignment::with(['store','product'])->find($consignment_id);
+    
+            // $consignment->store->store_name = $request->store_name;
+            // $consignment->store->save();
+            $consignment->store->update(['store_name'=>$request->store_name]);
+    
+            // $consignment->product->product_name = $request->product_name;
+            // $consignment->product->save();
+            $consignment->product->update(['product_name'=>$request->product_name]);
+            $consignment->product->update(['price'=>$request->price]);
+    
+            $consignment->entry_date = $request->entry_date;
             $consignment->exit_date = $request->exit_date;
-        }
-        $consignment->save();
-        return redirect('/transaksi');
+            $consignment->sold = $request->sold;
+            $consignment->quantity = $request->quantity;
+    
+            $consignment->save();
+            return redirect('/transaksi');
     }
 
 
